f817bd0209db79903b5b1a095c7935ca788ed22b
alraha <92431070+alraha@users.noreply.github.com>
2024-02-24 00:34:19 +0530

    Update equilibrium-point.cpp

--- Easy/Equilibrium Point/equilibrium-point.cpp
+++ Easy/Equilibrium Point/equilibrium-point.cpp
@@ -35,7 +35,29 @@ class Solution{
             
         //     if(sumright == sumleft) return i+1;
         // }
+
+
+        //approach 2
         
+       // int sum = 0; // initialize sum of whole array 
+	//int leftsum = 0; // initialize leftsum 
+
+	/* Find sum of the whole array */
+	//for (int i = 0; i < n; ++i) 
+		//sum += arr[i]; 
+
+	//for (int i = 0; i < n; ++i) 
+	//{ 
+		//sum -= arr[i]; // sum is now right sum for index i 
+
+	//	if (leftsum == sum) 
+		//	return i; 
+
+		//leftsum += arr[i]; 
+//	} 
+
+	/* If no equilibrium index found, then return 0 */
+//	return -1; 
         int leftarray[n] = {0};
         int rightarray[n] = {0};
 
