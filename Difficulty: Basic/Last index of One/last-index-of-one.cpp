ace8f2d7b60299780af1b11b28f49dc3e1767c8d
alraha <92431070+alraha@users.noreply.github.com>
2024-08-04 13:34:54 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Basic/Last index of One/last-index-of-one.cpp
+++ Difficulty: Basic/Last index of One/last-index-of-one.cpp
@@ -4,20 +4,19 @@ using namespace std;
 
 
 // } Driver Code Ends
-class Solution{
-    public:
-    int lastIndex(string s) 
-    {
-       for(int i=s.length()-1;i>=0;i--)
-       {
-           if(s[i]=='1')
-           {
-               return i;
-           }
-       }
-       return -1;
+class Solution {
+  public:
+    int lastIndex(string s) {
+        
+        for(int i= s.length()-1;i>=0;i--)
+        {
+            if(s[i]=='1')
+            {
+                return i;
+            }
+        }
+        return -1;
     }
-
 };
 
 //{ Driver Code Starts.
