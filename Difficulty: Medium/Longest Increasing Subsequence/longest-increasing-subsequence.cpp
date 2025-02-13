df1e3ab6abf975ee880b3e4b29b11c58b40129b4
alraha <92431070+alraha@users.noreply.github.com>
2025-02-14 00:11:42 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Longest Increasing Subsequence/longest-increasing-subsequence.cpp
@@ -0,0 +1,57 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+
+
+class Solution {
+  public:
+    // Function to find length of longest increasing subsequence.
+    int lis(vector<int>& arr) {
+        // code here
+        
+           int n = arr.size();
+        vector<int>v(n,1);
+        int maxa = 1;
+        for(int i=1;i<n;i++){
+            for(int j=0;j<i;j++){
+                if(arr[j]<arr[i]){
+                    v[i] = max(v[i],1+v[j]);
+                    maxa = max(maxa,v[i]);
+                }
+            }
+        }
+        return maxa;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    cin.ignore(); // to ignore the newline after the integer input
+
+    while (t--) {
+        int n;
+        vector<int> arr;
+        string input;
+
+        // Input format: first number n followed by the array elements
+        getline(cin, input);
+        stringstream ss(input);
+        int num;
+        while (ss >> num)
+            arr.push_back(num);
+
+        Solution obj;
+        cout << obj.lis(arr) << endl;
+        cout << "~" << endl;
+    }
+
+    return 0;
+}
+
+// } Driver Code Ends
